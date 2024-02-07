03cf30e78d27023f424ac259653cd08acf5fb061
CSIRamone <cidinei_inacio@hotmail.com>
2024-02-07 12:32:04 -0300

    implementacao service login interface

---
+++ fe_lab_clinicas_core/lib/fe_lab_clinicas_core.dart
@@ -0,0 +1,12 @@
+library fe_lab_clinicas_core;
+
+export 'src/fp/either.dart';
+export 'src/fp/nil.dart';
+export 'src/fp/unit.dart';
+export 'src/helpers/messagens.dart';
+export 'src/constants/local_storage_constants.dart';
+export 'src/restclient/rest_client.dart';
+export 'src/lab_clinicas_core_config.dart';
+export 'src/exceptions/auth_exception.dart';
+export 'src/exceptions/repository_exception.dart';
+export 'src/exceptions/service_exception.dart';
